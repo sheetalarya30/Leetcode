cc0e702821d00b5a63a1cc0e884262e8e9018eb0
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-02 20:53:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.75%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,11 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        N = len(heights)
+        #expected = heights --> why this will not work?
+        expected = heights[0:N]
+        expected.sort()
+        count = 0
+        for i in range(0, N):
+            if expected[i] != heights[i]:
+                count += 1
+        return count
