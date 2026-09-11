061bb7175130480ab5b8b35773b6e680dc1e5b1d
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-11 19:42:06 +0530

    Time: 399 ms (39.98%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,17 +1,10 @@
 SELECT name
-
 FROM Employee
-
 WHERE id IN (
-
     SELECT managerId
-
     FROM Employee
-
     WHERE managerId IS NOT NULL
-
     GROUP BY managerId
-
     HAVING COUNT(*) >= 5
 
 )
