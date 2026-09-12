5a23e9e4157eb286e15f42c9874ec990b066ce02
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-12 12:39:44 +0530

    Time: 885 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,6 @@
-select distinct author_id as id
+# Write your MySQL query statement below
+select author_id as id
 from Views
 where author_id=viewer_id
-order by id asc;
+group by author_id
+order by id;
