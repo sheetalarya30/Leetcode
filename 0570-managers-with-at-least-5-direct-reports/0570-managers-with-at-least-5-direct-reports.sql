0c4e7ed58a625e6a7033d9f13f497e525e74d8c4
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-13 12:53:43 +0530

    Time: 310 ms (99.47%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,9 @@
-SELECT name
-FROM Employee
-WHERE id IN (
-    SELECT managerId
-    FROM Employee
-    WHERE managerId IS NOT NULL
-    GROUP BY managerId
-    HAVING COUNT(*) >= 5
 
-)
+SELECT m.name
+FROM Employee e
+JOIN Employee m
+ON e.managerId = m.id
+GROUP BY m.id, m.name
+HAVING COUNT(*) >= 5;
+
+
