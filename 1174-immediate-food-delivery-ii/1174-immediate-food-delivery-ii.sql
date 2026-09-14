2d7a079e03947e1e298b6337b0c90809ea4f832c
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-14 18:43:30 +0530

    Time: 785 ms (31.12%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,8 +1,7 @@
-SELECT 
-    ROUND(AVG(order_date = customer_pref_delivery_date) * 100, 2) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id, order_date) IN (
-    SELECT customer_id, MIN(order_date)
-    FROM Delivery
-    GROUP BY customer_id
-);
+select round(avg(order_date=customer_pref_delivery_date)*100,2) as immediate_percentage
+from Delivery
+where (customer_id,order_date) in(
+      select customer_id,min(order_date)
+      from Delivery
+      group by customer_id
+) ;
