967e79f7ef370db148a552e4063a2681487552f7
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-17 10:51:21 +0530

    Time: 474 ms (93.29%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,10 +1,4 @@
-SELECT 
-    activity_date AS day, 
-    COUNT(DISTINCT user_id) AS active_users
-FROM 
-    Activity
-WHERE 
-    activity_date > '2019-06-27' 
-    AND activity_date <= '2019-07-27'
-GROUP BY 
-    activity_date;
+select activity_date as day, count(distinct user_id) as active_users
+from Activity
+where activity_date between '2019-06-28' and '2019-07-27'
+group by activity_date;
