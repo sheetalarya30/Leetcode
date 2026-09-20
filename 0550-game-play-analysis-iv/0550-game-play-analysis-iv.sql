5660222468ecbf0e0d886d2cd3fe6f215a0a94ad
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-20 18:16:22 +0530

    Time: 730 ms (27.03%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,29 +1,22 @@
-
--- select round(avg(datediff(a.event_date,b.event_date)=1),2) as fraction
--- from Activity a
--- join Activity b
--- on a.player_id=b.player_id;
-
-
-
--- select  from
--- (select,
--- lag(event_date) over(order by event_date) as fraction
--- from Activity);
-
-
-
-
-SELECT 
-    ROUND(AVG(a.event_date IS NOT NULL), 2) AS fraction
-FROM (
-    SELECT player_id, MIN(event_date) AS first_date
-    FROM Activity
-    GROUP BY player_id
+select round(avg (a.event_date is not null),2) as fraction
+from (
+    select player_id,min(event_date) as first_date
+    from Activity
+    group by player_id
 ) f
-LEFT JOIN Activity a 
-  ON f.player_id = a.player_id 
- AND DATEDIFF(a.event_date, f.first_date) = 1;
+left join Activity a
+on a.player_id=f.player_id
+and datediff(a.event_date,f.first_date)=1;
+
+
+
+
+
+
+
+
+
+
 
 
 
