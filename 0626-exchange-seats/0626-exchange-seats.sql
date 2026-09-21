c54b358ae03f90f50370fc474d3b6b2bded68e1f
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-21 11:07:24 +0530

    Time: 401 ms (42.96%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,10 +1,8 @@
-# Write your MySQL query statement below
-SELECT 
-    CASE 
-        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
-        WHEN id % 2 = 1 THEN id + 1
-        ELSE id - 1
-    END AS id,
-    student
-FROM Seat
-ORDER BY id;
+select 
+     case
+         when id%2=1 and id =(select max(id) from Seat) then id
+         when id%2=1 then id+1
+         else id-1
+         end as id,Student
+         from Seat
+order by id;
