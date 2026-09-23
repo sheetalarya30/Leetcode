ed5dbec4ce82d5c046ad628f9e79d9a08b8c8700
Sheetal Arya <aryasheetal9934@gmail.com>
2026-09-23 09:44:32 +0530

    Time: 845 ms (78.82%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,10 +1,7 @@
-DELETE FROM Person
-WHERE id NOT IN (
-    SELECT id
-    FROM (
-        SELECT MIN(id) AS id
-        FROM Person
-        GROUP BY email
-    ) t
-);
+delete p1
+from Person p1
+join Person p2
+on p1.email=p2.email
+and p1.id>p2.id;
+
 
